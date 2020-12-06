1df729ee8732e7cdb68c3a2d91d61b8544e1198c
Sqzi00 <61646294+Sqzi00@users.noreply.github.com>
2020-12-06 08:25:47 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,10 @@
+/2. Zmienne i stałe
+// Zadanie 1:
+
+let number = 2;
+let string = "dwa";
+let stringNumConcat = number + string;
+let logicValue = 3 < 2;
+let special = null;
+
+console.log(number, string, stringNumConcat, logicValue, special);
