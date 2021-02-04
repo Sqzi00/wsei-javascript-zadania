f4539d0a05102c1f0f3b35e98c02e4b676a324c3
Sqzi00 <61646294+Sqzi00@users.noreply.github.com>
2021-02-04 21:36:26 +0100

    Update script.js

--- 2. Zmienne i stałe/script.js
+++ 2. Zmienne i stałe/script.js
@@ -8,3 +8,16 @@ let logicValue = 3 < 2;
 let special = null;
 
 console.log(number, string, stringNumConcat, logicValue, special);
+
+// Zadanie 2
+
+const a = 2;
+const b = 4;
+let sum = 0;
+sum = a+b;
+console.log(sum);
+
+// Z A D A N I E  3
+let x;
+console.log(x);
+//undefined, ponieważ nie ma przypisanej wartości.
