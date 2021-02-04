27c8605494c6bf14cccb2b71b60048a7ff556795
Sqzi00 <61646294+Sqzi00@users.noreply.github.com>
2021-02-04 21:52:26 +0100

    Update script.js

--- 3.Operatory/script.js
+++ 3.Operatory/script.js
@@ -1,7 +1,48 @@
-//
-zadanie 1
-let a = true ;
-let b = false;
+// zadani 1
+let x = true ;
+let y = false;
 console.log(a==b);
 
-// zad 2
+// zadanie 2
+
+let x = 2;
+let y = 4;
+let moduloResult = 0;
+moduloResult = x % y;
+console.log(moduloResult);
+
+//zadanie 3
+
+let string1 = "first";
+let string2 = "second";
+let stringsResult = "";
+
+stringsResult = string1 + string2;
+
+console.log(stringsResult);
+
+// zadanie 4
+
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber == someString); // operator == porównuje wartosci
+console.log(someNumber === someString); // === wartości oraz typy
+
+//zadanie 5
+let counter = 30;
+console.log(counter);
+counter++;
+console.log(counter);
+counter--;
+console.log(counter);
+// zadanie 6
+
+let num1 = 9;
+let num2 = 5;
+let result = 0;
+
+result = num1 > num2;
+console.log(result);
+
+///
