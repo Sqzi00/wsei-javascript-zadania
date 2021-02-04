3155a501421af8f2b1d26b4053526c787b88e27d
Sqzi00 <61646294+Sqzi00@users.noreply.github.com>
2021-02-04 23:43:33 +0100

    Update script.js

--- 6.Funkcje/script.js
+++ 6.Funkcje/script.js
@@ -6,3 +6,34 @@ function sayHi() {
 sayHi();
 
 // zadanie  2
+function changer(param) {
+    console.log(param);
+}
+
+changer('test param');
+
+// zadanie 3
+const x = [1, 2, 3, 4, 5];
+function table(x){
+  return x;
+}
+console.log(table(x));
+
+// zad 4
+
+function myString(str) {
+    let i = 0;
+    const timer = setInterval(() => {
+        if(i < 5) {
+            console.log(str);
+            i++;
+        }
+        else{
+            console.log("Koniec");
+            clearInterval(timer);
+        } 
+    }, 3000);
+}
+
+
+myString("Counting...");
