5e404ed1b1e516b8e824ffad298cd751fcd152d8
Sqzi00 <61646294+Sqzi00@users.noreply.github.com>
2021-02-05 00:47:12 +0100

    Update script.js

--- 7. Tablice/script.js
+++ 7. Tablice/script.js
@@ -88,4 +88,45 @@ show1([2,3,4,5,6]);
 console.log(show2([3,4,5,6,7]));
   
   // zad 7
-  
+  function show3(tab){
+    return (tab.sort((a, b) => a - b));
+}
+console.log(show3([4,15,6,27,8,1]));
+
+// zad 8
+    const table1 = [11, 12, 13];
+const table2 = [14, 15, 16];
+function create(table1, table2)
+{
+  var x = 0;
+  const table3 = [];
+
+  for(var a = x; a <= table1.length - 1; a++)
+  {
+    table3[x] = table1[x] + table2[x];
+    x++;
+  }
+    console.log(table3);
+}
+create(table1, table2);
+    
+    // zad 9
+    
+    function show4(a,b){ 
+        return a.filter(c => c != b);
+}
+console.log(show4([5,6,7,8,9],7));
+
+    // zad 10
+    const tab = [-1, 3, 10, -23, 5];
+var counter = 0;
+function changer(tab)
+{
+  for( var a = counter; a < tab.length; a++)
+  {
+      tab[a] = (tab[a] - (2*tab[a]));
+    var arr = tab;
+  }
+  console.log(arr);
+}
+changer(tab);
