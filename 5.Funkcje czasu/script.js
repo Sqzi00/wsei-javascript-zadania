ef83b097abd6ced85b2d21ebf56a4350913e6bca
Sqzi00 <61646294+Sqzi00@users.noreply.github.com>
2021-02-04 23:05:45 +0100

    Update script.js

--- 5.Funkcje czasu/script.js
+++ 5.Funkcje czasu/script.js
@@ -1,9 +1,28 @@
 // zad 1
-let lime = 0 ;
+let time = 0 ;
 const interval = setInterval(() => {
 time +=1;
-console.log('Cześć po raz ${time}');
+console.log(`Cześć po raz ${time}`);
 if(time >=15){
 clearInterval(interval);
 }
-},1000);
+},3000);
+
+// zad 2
+
+let myArray = [1,2,3,4];
+let myTimer = setTimeout(() => {
+    for (let i=0; i < myArray.length; i++){
+        console.log(myArray[i]);
+    }
+    let j=0
+    let myInterval = setInterval(() => {
+        console.log(myArray[j]);
+        j++
+        if(j>= myArray.lenght) {
+            clearInterval(myInterval);
+        }
+    },3000)
+},2000 )
+
+///
