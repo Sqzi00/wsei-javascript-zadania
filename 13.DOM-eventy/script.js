3ce5af873ffc5bffae5f383535672e848d237a11
Sqzi00 <61646294+Sqzi00@users.noreply.github.com>
2021-02-05 10:57:01 +0100

    Update script.js

---
+++ 13.DOM-eventy/script.js
@@ -0,0 +1,16 @@
+//zad 1
+//a
+      document.querySelector('#test-event').addEventListener('click', (event) => console.log(event));
+ // b
+      document.addEventListener('mousemove', (event) => console.log(event));
+ // c
+      document.querySelector('#test-event').addEventListener('mouseover', (event) => console.log(event));
+  // d
+      document.addEventListener('keypress', (event) => console.log(event));
+  //e
+      document.addEventListener('scroll', (event) => console.log(event));
+ // f
+      document.querySelector('#input-test').addEventListener('change', (event) => console.log(event));
+      
+  //zad 2
+  
