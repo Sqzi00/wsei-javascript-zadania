e3f4780796295b2f67cd19e8dad3c26a013cee8f
Sqzi00 <61646294+Sqzi00@users.noreply.github.com>
2021-02-05 12:34:24 +0100

    Update script.js

--- 13.DOM-eventy/script.js
+++ 13.DOM-eventy/script.js
@@ -92,3 +92,24 @@ function redChange(){
 redChange()
 
 // zad 7
+var ourInput = document.querySelector('#calculator > input');
+    var clearFlag = false;
+    var another = false;
+
+    document.querySelectorAll('#calculator > div > button').forEach((element) => {
+        element.addEventListener('click', (event) => {
+            if (clearFlag == true) {
+                ourInput.value = "";
+                clearFlag = false;
+            }
+            ourInput.value = ourInput.value + event.target.innerText;
+            if (another == true) {
+                ourInput.value = eval(ourInput.value);
+                another = false;
+                clearFlag = true;
+            }
+            if (['/', '*', '+', '-'].includes(event.target.innerText)) {
+                another = true;
+            }
+        });
+    });
