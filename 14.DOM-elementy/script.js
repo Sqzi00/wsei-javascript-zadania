65d76493e5a9224bebe8900de711933272dfe080
Sqzi00 <61646294+Sqzi00@users.noreply.github.com>
2021-02-05 13:20:06 +0100

    Update script.js

--- 14.DOM-elementy/script.js
+++ 14.DOM-elementy/script.js
@@ -11,3 +11,87 @@ function listDivs(paramArr){
 console.log(listDivs(divs));
 
 // zad 2
+function list(param){
+    console.log(param.innerHTML);
+    console.log(param.outerHTML);
+    console.log(param.className);
+    console.log([...param.classList]);
+    console.log(param.dataset);
+
+}
+list(document.querySelector(".short-list"))
+
+//zad 3
+
+function numbers(...int) {
+        return {
+            sum: int.reduce((prev, curr) => parseInt(prev) + parseInt(curr)),
+
+            difference: int.reduce((prev, curr) => prev - curr),
+        }
+    }
+
+    let numb = document.querySelector('#datasetCheck').dataset;
+    console.log(numbers(numb.numberone, numb.numbertwo, numb.numberThree));
+
+// zad 4
+
+document.getElementById("spanText").innerText = "dowolnny";
+
+// zad 5
+
+document.getElementById("spanText").className = "newClassName";
+// zad 6
+var element = document.getElementById("classes");
+var list = element.classList;
+var concat = [];
+for (let i=0 ; i< list.length; i++)
+{
+    concat.push(list[i]);
+    console.log(list[i]);
+}
+console.log(concat.join('+'));
+element.className= '';
+
+// zad 7
+
+const list2 = document.querySelectorAll('#longList>li')
+function addAttributes(elements){
+    elements.forEach(element => {
+        if(!element.getAttribute("data-text")){
+            element.dataset.text = "text";
+        }
+    })
+}
+const result =addAttributes(list2)
+
+// zad 8
+
+function saveString(string) {
+  const obj = {
+    newClass: string
+  }
+  return obj;
+}
+
+function addNewClass(obj) {
+  const newClass = obj.newClass;
+  document.getElementById('myDiv').classList.add(newClass);
+}
+
+addNewClass(saveString('string_z_parametru'));
+function saveString(string) {
+  const obj = {
+    newClass: string
+  }
+  return obj;
+}
+
+function addNewClass(obj) {
+  const newClass = obj.newClass;
+  document.getElementById('myDiv').classList.add(newClass);
+}
+
+addNewClass(saveString('string_z_parametru'));
+
+// zad 9
